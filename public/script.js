3c8eb13294a4e6528c07406617d2946251d13236
victor <victor.velazquez2210@gmail.com>
2026-04-21 08:23:36 -0600

    Initial commit - first pipeline project

---
+++ public/script.js
@@ -0,0 +1,16 @@
+const messages = [
+"Keep going. You are learning something valuable.",
+"Small progress is still progress.",
+"Every expert started as a beginner.",
+"Practice makes your skills stronger.",
+"Your future projects start with simple steps like this one.",
+"tung be with you twin."
+];
+
+const button = document.getElementById("messageBtn");
+const messageBox = document.getElementById("message");
+
+button.addEventListener("click", () => {
+const randomIndex = Math.floor(Math.random() * messages.length);
+messageBox.textContent = messages[randomIndex];
+});
